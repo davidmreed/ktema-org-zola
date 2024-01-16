ddf9405929a9c6a77eaffa8d05924a1c22dda0ea
David Reed <david@ktema.org>
2024-01-15 23:55:30 -0700

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "my project description";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/23.05"; };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
   outputs = { self, nixpkgs  }:
         let pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
 	in {
