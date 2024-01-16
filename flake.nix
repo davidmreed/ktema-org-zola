cf00735897b55e5e5118dc54934e1e84703cac5c
David Reed <david@ktema.org>
2024-01-15 23:22:36 -0700

    Add flake for development

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "my project description";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/23.05"; };
+  outputs = { self, nixpkgs  }:
+        let pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+	in {
+          devShells.x86_64-linux.default = pkgs.mkShell {
+	    buildInputs = with pkgs; [
+	      zola
+	      just
+	    ];
+	  };
+        };
+}
